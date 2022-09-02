ff772cda2acd9bf618a24b511c13356445f6c805
FerdinandYeke <112411799+FerdinandYeke@users.noreply.github.com>
2022-09-02 15:53:43 -0400

    Add files via upload

---
+++ FirstNameLastNameHobbyMajor.java
@@ -0,0 +1,18 @@
+//Ferdinand Yeke
+//Bellarmine University
+//CS130-2
+//9/2/2022
+//FirstNameLastNameHobbyMajor.java
+
+//This program prints out my first name, last name, hobby, & major.
+
+public class FirstNameLastNameHobbyMajor
+{
+	public static void main(String []args)
+	{
+		for(String Info: args)
+		{
+		System.out.println("My First name, Last Name, Hobby, and Major: " +Info);
+		}
+	}//Main Method ends here
+}//Class ends here
